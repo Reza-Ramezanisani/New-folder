e0e8787f754d0220340ab503a3bee36816b080dc
Reza <ramezanisanir@gmail.com>
2021-08-04 09:53:45 +0430

    fixed bar

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-function my() {
-    var input,ul,li,a,i;
-    input=document.getElementById("mysearch").value.toUpperCase();
-    ul=document.getElementsByClassName("list")[0];
-    
-    li=ul.getElementsByTagName("li");
-    for (let i = 0; i < li.length; i++) {
-         a= li[i].getElementsByTagName("a")[0];
-         if(a.innerText.toUpperCase().indexOf(input)>-1){
-             a.style.display="";
-         }
-        else{
-            a.style.display="none";
-        }
-    }
+var main=document.getElementById("main");
+var side=document.getElementById("side");
+function openside(){
+    side.style.width="100%";
+    main.style.marginLeft="0";
+
+}
+function closeside() {
+    main.style.marginLeft="0";
+    side.style.width='0%';
 }
 
 
